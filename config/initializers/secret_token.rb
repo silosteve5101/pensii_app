e4f325c256463898bff906a573c849a0de710b55
Zala Stefan <silosteve5101@yahoo.com>
2011-12-29 00:06:21 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PensiiApp::Application.config.secret_token = 'a364bb7c01dfa5a34bf3866e7cb2d4b8b5db6c3c1c0e050aa67c075da338fcac1e8c0018314d5dd11126bb81b857f40ece458ab2bb432aafb653896e47cbe8ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PensiiApp::Application.config.session_store :cookie_store, :key => '_pensii_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PensiiApp::Application.config.session_store :active_record_store
